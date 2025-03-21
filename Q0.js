3604608090da77820ee27d575cfb9a83e7b815cb
nminhthai <n.minhthai0713@gmail.com>
2025-03-21 11:57:42 +0700

    D3_2

--- Q0.js
+++ Q0.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var q0Div = document.getElementById("Q0");
     if (q0Div) {
         var textElement = document.createElement("p");
-        textElement.textContent = "[2211124029140 Nguyễn Minh Thái 48K29.1]";
+        textElement.textContent = "[221124029140 Nguyễn Minh Thái 48K29.1]";
         textElement.style.textAlign = "left";
         textElement.style.color = "black";
         textElement.style.fontFamily = "'Arial', 'Helvetica', sans-serif"; 
